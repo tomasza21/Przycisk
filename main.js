185cdc43d81fc7d326daafb88e9aa3a69632a2d8
tomasza21 <101091227+tomasza21@users.noreply.github.com>
2022-03-18 09:31:12 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 const wynik = document.getElementById('wynik');
 
-// wynik.innerHTML = "Test";
+wynik.innerHTML = "Test";
+function zmiana(){
+let element = document.getElementById('button')
+if(element.className=="on"){
+    element.className="off"
+    element.value="off"
+    //element.style.color="YY"
+}else {
+    element.className="on"
+    element.value="on"
+    //element.style.color
+}
+}
+
+function zmiana1(){
+    let element = document.getElementsByClassName('test1')
+    if(element.value=="on"){
+        element.className="test2"
+        element.value="off"
+    }else{
+        element.className="test1"
+        element.value="on"
+    }
+}
